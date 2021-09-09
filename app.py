00d16bde9e4424d6c0fc8cfa594d33772efcb717
Sebastian Rodriguez Vasquez <seb.rodriguezvasquez@gmail.com>
2021-09-09 15:03:40 +0200

    First Commit

--- app.py
+++ app.py
@@ -8,46 +8,29 @@ import requests
 import tempfile
 from PIL import Image
 
-'''
-# CoachAI
-'''
+st.title('Coach AI')
 
-st.markdown('''
-Hello World !
-''')
+st.sidebar.header('Upload your video')
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
+video = st.sidebar.file_uploader('Video File',
+                                 type=["mp4", "mov", 'avi', 'asf', 'm4v'])
 
-
-class VideoTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.landmarks = {}
-        self.i = 0
-
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-            results = pose.process(img)
-            # self.landmarks[i] = results.
-            mp_drawing.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-
-        return img
-
-webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
-
-stframe = st.empty()
-video = st.file_uploader('video')
-tfile = tempfile.NamedTemporaryFile(delete=False)
 if video:
     url = 'https://coachai-ujeungn6tq-ew.a.run.app/predict'
     data = {'video': video}
-    response = requests.post(url, files=data).json()
-    prediction = response[0]
-    repetitions = response[1]
-    st.write(f'Your exercise: {prediction}')
-    st.write(f'You did: {repetitions} reps')
+    x = requests.post(url, files=data)
+    st.write(x.text)
 
-    st.video(video)
+    col1, col2 = st.columns(2)
+
+    col1.header('lisa a la pression')
+
+    col2.header('maxime a la pression')
+
+    st.text('')
+
+    col3, col4 = st.columns(2)
+
+    col3.header('Seb a la pression')
+
+    col4.header('maxime a la pression')
